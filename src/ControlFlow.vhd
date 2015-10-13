a9472e9e320b0bc65a2b923abeb00528e272322e
Øyvind Kjerland <oyvinkje@stud.ntnu.no>
2015-10-13 17:15:18 +0200

    Fix signal types in ControlFlow module Testbench for ControlFlow reports success

--- src/ControlFlow.vhd
+++ src/ControlFlow.vhd
@@ -15,34 +15,48 @@ entity ControlFlow is
 		clk, reset : in std_logic;
 		pc_out : out std_logic_vector(ADDR_WIDTH-1 downto 0);
 		instruction_in : in std_logic_vector(DATA_WIDTH-1 downto 0);
-		alu_zero_in, branch_in, jump_in : in std_logic
+		alu_zero_in, branch_in, jump_in, pc_write_in : in std_logic
 	);
 
 end ControlFlow;
 
 architecture Behavioral of ControlFlow is
+
+	signal pc, pc_in, pc_add_1, pc_jump, pc_immediate_signextend, pc_add_immediate, pc_branch : signed(DATA_WIDTH-1 downto 0);
 	
-	signal pc, pc_in, pc_add_1, pc_jump, pc_imm_signex, pc_add_imm, pc_branch: std_logic_vector(DATA_WIDTH-1 downto 0);
 begin
 
-
-	PCBehaviour: process is
+	-- Process for PC register
+	PCBehaviour: process(clk, reset)
 	begin
-		if reset = '0' then
+		if reset = '1' then
 			pc <= (others => '0');
-		elsif rising_edge(clk) then
+		elsif rising_edge(clk) and pc_write_in = '1' then
 			pc <= pc_in;
 		end if;
 	end process;
 	
+	-- Increment PC with 1
 	pc_add_1 <= pc + 1;
-	pc_jump <= pc(31 downto 26) & instruction_in(25 downto 0);
-	pc_imm_signex <= std_logic_vector(resize(signed(instruction(15 downto 0)), DATA_WIDTH));
-	pc_add_imm <= pc_imm_signex + pc_add_1;
-	pc_branch <= pc_add_1 when (branch_in = '1' and alu_zero_in = '1')
-		else pc_add_imm;
+	
+	-- Concat pc[31-26] and instruction[25-0] to get jump address
+	pc_jump <= signed(std_logic_vector(pc(31 downto 26)) & instruction_in(25 downto 0));
+	
+	-- Use resize on signed to sign extend immediate value
+	pc_immediate_signextend <= (resize(signed(instruction_in(15 downto 0)), DATA_WIDTH));
+	
+	-- Immediate + PC + 1
+	pc_add_immediate <= pc_immediate_signextend + pc_add_1;
+	
+	-- if zero signal on ALU and branch signal are set, add immediate as offset, else increment with 1 
+	pc_branch <= pc_add_immediate when (branch_in = '1' and alu_zero_in = '1')
+		else pc_add_1;
+	
+	-- Choose jump address if jump signal is set
 	pc_in <= pc_jump when jump_in = '1' else pc_branch;
-	pc_out <= pc(ADDR_WIDTH-1 downto 0);
+	
+	-- Cut the address for instruction memory to the correct address width
+	pc_out <= std_logic_vector(pc(ADDR_WIDTH-1 downto 0));
 
 end Behavioral;
 
