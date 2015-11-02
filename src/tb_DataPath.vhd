7728a6bce2958e8dbb6d3c99bfb84b639a13ca95
Truls Rustad Fossum <trulfos@gmail.com>
2015-11-02 20:04:27 +0100

    Pipelined testbench for data path

--- src/tb_DataPath.vhd
+++ src/tb_DataPath.vhd
@@ -25,7 +25,8 @@ ARCHITECTURE behavior OF tb_DataPath IS
          reg_src_in : IN  reg_src_t;
          alu_1_out : OUT  std_logic_vector(31 downto 0);
          alu_2_out : OUT  std_logic_vector(31 downto 0);
-         write_data_out : OUT  std_logic_vector(31 downto 0)
+         write_data_out : OUT  std_logic_vector(31 downto 0);
+			mem_addr_out : OUT std_logic_vector(31 downto 0)
         );
     END COMPONENT;
     
@@ -39,6 +40,7 @@ ARCHITECTURE behavior OF tb_DataPath IS
    signal immediate_in : std_logic_vector(15 downto 0) := (others => '0');
    signal read_data_in : std_logic_vector(31 downto 0) := (others => '0');
    signal alu_result_in : std_logic_vector(31 downto 0) := (others => '0');
+	signal mem_addr_out : std_logic_vector(31 downto 0) := (others => '0');
    signal reg_write_in : boolean := false;
    signal alu_src_in : alu_src_t := ALU_SRC_REGISTER;
    signal reg_src_in : reg_src_t := REG_SRC_ALU;
@@ -68,7 +70,8 @@ BEGIN
           reg_src_in => reg_src_in,
           alu_1_out => alu_1_out,
           alu_2_out => alu_2_out,
-          write_data_out => write_data_out
+          write_data_out => write_data_out,
+          mem_addr_out => mem_addr_out
         );
 
    -- Clock process definitions
@@ -79,11 +82,29 @@ BEGIN
 		clk <= '1';
 		wait for clk_period/2;
    end process;
+
+
+
  
 
    -- Stimulus process
    stim_proc: process
-   begin		
+		-- Checks all regs are zero
+		procedure AssertRegsZero is
+		begin
+			for i in 0 to 31 loop
+				read_reg_1_in <= std_logic_vector(to_unsigned(i,5));
+				for j in 0 to 31 loop
+					read_reg_2_in <= std_logic_vector(to_unsigned(j,5));
+					wait for clk_period;
+					assert alu_1_out = x"00000000" and alu_2_out = x"00000000"
+						report "all registers should be 0 after reset"
+						severity failure;
+				end loop;
+			end loop;
+		end procedure;
+   begin
+	
       -- hold reset state for 100 ns.
 		reset <= '1';
       wait for 100 ns;	
@@ -92,17 +113,7 @@ BEGIN
 		-- Set ALU src to output read data 2
 		alu_src_in <= ALU_SRC_REGISTER;
 		
-		-- Registers should now be zero
-		for i in 0 to 31 loop
-			read_reg_1_in <= std_logic_vector(to_unsigned(i,5));
-			for j in 0 to 31 loop
-				read_reg_2_in <= std_logic_vector(to_unsigned(j,5));
-				wait for clk_period;
-				assert alu_1_out = x"00000000" and alu_2_out = x"00000000"
-					report "all registers should be 0 after reset"
-					severity failure;
-			end loop;
-		end loop;
+		AssertRegsZero;
 		
 		-- Try to fill registers with values when RegWrite is 0
 		for i in 1 to 31 loop
@@ -114,17 +125,7 @@ BEGIN
 		end loop;
 		
 		-- Registers should still be zero
-		for i in 0 to 31 loop
-			read_reg_1_in <= std_logic_vector(to_unsigned(i,5));
-			for j in 0 to 31 loop
-				read_reg_2_in <= std_logic_vector(to_unsigned(j,5));
-				wait for clk_period;
-				assert alu_1_out = x"00000000" and alu_2_out = x"00000000"
-					report "Registers should not update value while reg_write_in is false"
-					severity failure;
-			end loop;
-		end loop;
-		
+		AssertRegsZero;
 			
 		-- Fill registers with values when RegWrite is 1
 		for i in 1 to 31 loop
@@ -151,6 +152,28 @@ BEGIN
 					severity failure;
 			end loop;
 		end loop;
+
+		-- Check that value is held for a clock cycle
+		for i in 1 to 31 loop
+			read_reg_1_in <= std_logic_vector(to_unsigned(i,5));
+			for j in 0 to 31 loop
+				read_reg_2_in <= std_logic_vector(to_unsigned(j,5));
+
+				-- Simulate rising edge
+				wait for 3*clk_period/5;
+
+				-- Change addresses, wait and check again
+				read_reg_1_in <= std_logic_vector(to_unsigned(i + 1, 5));
+				read_reg_2_in <= std_logic_vector(to_unsigned(j + 1, 5));
+
+				wait for 2*clk_period/5;
+
+				assert alu_1_out = std_logic_vector(to_unsigned(i,32)) and alu_2_out = std_logic_vector(to_unsigned(j,32))
+					report "alu out should be held for a clock cycle"
+					severity failure;
+
+			end loop;
+		end loop;
 		
 		
 		-- Try to write a value in register 0
@@ -171,42 +194,83 @@ BEGIN
 		read_reg_2_in <= "00100"; -- Register 4, should have value 4
 		immediate_in <= x"1001"; -- Immediate value 
 		wait for clk_period;
-		
+
 		-- alu_2_out should be sign extended immediate_in
 		assert alu_2_out = std_logic_vector(resize(signed(immediate_in),32))
 			report "alu_2_out should be sign extended immediate in"
 			severity failure;
-			
+
+		-- Check immediate delay
+		immediate_in <= x"1000";
+		wait for 2*clk_period/5; -- No rising edge
+		assert alu_2_out = x"00001001"
+			report "alu_2_out should be sign extended immediate in"
+			severity failure;
+
+		wait for 3*clk_period/5; -- Rising edge
+		assert alu_2_out = std_logic_vector(resize(signed(immediate_in),32))
+			report "alu_2_out should be sign extended immediate in"
+			severity failure;
+
+
+
 		-- write_data_out should be register 2 output
 		alu_src_in <= ALU_SRC_REGISTER;
 		for i in 0 to 31 loop
 			read_reg_2_in <= std_logic_vector(to_unsigned(i, 5));
-			wait for clk_period;
+			wait for 2*clk_period;
 			assert write_data_out = alu_2_out
-				report "write_data_out out should be the same as read data 2 when alu_src_in = ALU_SRC_REGISTER"
+				report "write_data_out out should be the same as read data 2 when alu_src_in = ALU_SRC_REGISTER with a cycle extra delay."
 				severity failure;
 		end loop;
 			
 		
-		-- Test MemToReg
-		alu_result_in <= x"00011000";
-		read_data_in <= x"11001100";
-		reg_src_in <= REG_SRC_MEMORY;
+		-- Test reg_src
+		reg_src_in <= REG_SRC_ALU;
 		read_reg_1_in <= "10000"; -- Read reg 16
 		write_reg_in <= "10000"; -- Write reg 16
+
+		for i in 0 to 200 loop
+			alu_result_in <= std_logic_vector(to_unsigned(i, 32));
+			wait for 3*clk_period/5; -- Rising edge crossed
+			assert mem_addr_out = alu_result_in
+				report "mem_addr should be alu_result delayed by one cycle"
+				severity failure;
+
+			alu_result_in <= std_logic_vector(to_unsigned(i + 20, 32));
+			wait for 2*clk_period/5; -- No edge crossing
+			assert mem_addr_out = std_logic_vector(to_unsigned(i, 32))
+				report "mem_addr should be held for one clock cycle"
+				severity failure;
+
+			wait for clk_period;
+			reg_write_in <= true;
+			wait for clk_period;
+
+			-- check reg output
+			assert to_integer(unsigned(alu_2_out)) = i
+				report "Correct alu result not written back"
+				severity failure;
+
+			reg_write_in <= false;
+
+		end loop;
+
+		reg_src_in <= REG_SRC_MEMORY;
 		reg_write_in <= true;
-		wait for clk_period;
-		reg_write_in <= false;
-		
-		
-		-- alu_1_out should now be read_data_in
-		assert alu_1_out = read_data_in
-			report "alu_1_out should be read_data_in when mem_to_reg is true"
-			severity failure;
-			
+		for i in 0 to 200 loop
+			read_data_in <= std_logic_vector(to_unsigned(i, 32));
+
+			wait for clk_period;
+
+			-- check reg output
+			assert to_integer(unsigned(alu_2_out)) = i
+				report "Correct memory data not written back"
+				severity failure;
+
+		end loop;
 
 		report "Test success";
-      wait;
    end process;
 
 END;
