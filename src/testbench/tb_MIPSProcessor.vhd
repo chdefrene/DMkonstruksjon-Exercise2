beeea7c20dacc1ce91ee26e353b856de2f4f3acc
Øyvind Kjerland <oyvinkje@stud.ntnu.no>
2015-11-13 15:16:35 +0100

    Update testbench for processor with delay slots

--- src/testbench/tb_MIPSProcessor.vhd
+++ src/testbench/tb_MIPSProcessor.vhd
@@ -111,7 +111,7 @@ DataMem:			entity work.DualPortMem port map (
 		end WriteInstructionWord;
 		
 		procedure FillInstructionMemory is
-			constant TEST_INSTRS : integer := 39;
+			constant TEST_INSTRS : integer := 45;
 			type InstrData is array (0 to TEST_INSTRS-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
 
 
@@ -121,8 +121,9 @@ DataMem:			entity work.DualPortMem port map (
 				X"8C010001", -- lw $1, 1($0)      /$1 = 1 
 				X"00211020", -- add $2, $1, $1    /$2 = 2
 				X"ac020002", -- sw $2, 2($0)      /Store 2 to address 2
-				X"10420001", -- beq $2, $2, 1     /Branching
-				X"ac010003", -- sw $1, 3($0)      /SKIPPED
+				X"10420002", -- beq $2, $2, 2     /Branching 2 to get to instruction 7
+				X"ac000001", -- sw $0, 1($0)      /Store 0 to address 1 -- This is a delay slot which will be executed
+				X"ac020001", -- sw $2, 1($0)      /SKIPPED
 				X"00411820", -- add $3, $2, $1    /$3 = 2+1 = 3 -- Test ALU operations and forwarding
 				X"00622022", -- sub $4, $3, $2    /$4 = 3-2 = 1
 				X"20850005", -- addi $5, $4, 5    /$5 = 1+5 = 6
@@ -145,51 +146,24 @@ DataMem:			entity work.DualPortMem port map (
 				X"AC0B000B", -- sw $11, 11($0)
 				X"AC0C000C", -- sw $12, 12($0)
 				X"AC0D000D", -- sw $13, 13($0)
-				X"10000002", -- beq $0, $0, 2 -- Test branching
-				X"10000003", -- beq $0, $0, 3
-				X"1000FFFE", -- beq $0, $0, -2
-				X"1000FFFE", -- beq $0, $0, -2
+				X"10000005", -- beq $0, $0, 5 -- Test branching
+				X"00000020", -- add $0, $0, $0    /Delay slot
+				X"10000006", -- beq $0, $0, 6
+				X"00000020", -- add $0, $0, $0    /Delay slot
+				X"1000FFFD", -- beq $0, $0, -3
+				X"00000020", -- add $0, $0, $0    /Delay slot
+				X"1000FFFD", -- beq $0, $0, -3
+				X"00000020", -- add $0, $0, $0    /Delay slot
 				X"AC000001", -- sw $0, 1($0)     /SKIPPED
-				X"08000024", -- j 36 -- Testing jump
-				X"AC000003", -- sw $4, 3($0)     /SKIPPED
+				X"0800002A", -- j 36 -- Testing jump
+				X"00000020", -- add $0, $0, $0    /Delay slot
+				X"AC000004", -- sw $0, 3($0)     /SKIPPED
 				X"AC000004", -- sw $0, 4($0)     /SKIPPED
-				X"8C010003", -- lw $1, 3($)
 				X"1000FFFF", --beq $0, $0, -1	/Branch back one step to hold off code at this spot
+				X"00000020", -- add $0, $0, $0    /Delay slot
 				X"AC050012" --sw $5, 18($0)		/SHOULD NEVER HAPPEN (Saving value 6 on address 18.)
 			);
 
---			variable TestInstrData : InstrData := (
---				X"8C010001", --lw $1, 1($0)		/$1 =  2
---				X"8C020002", --lw $2, 2($0)		/$2 = 10	
---				X"00221820", --add $3, $1, $2	   /$3 = 12		
---				X"AC030005", --sw $3, 5($0)		/Saving value 12 on address 5	
---				X"10000002", --beq $0, $0, 2		/Jumping to adress +2 = 8	
---				X"AC030003", --sw $3, 3($0)      /SKIPPED (Saving value 12 on address 3)			
---				X"AC030004", --sw $3, 4($0)		/SKIPPED	(Saving value 12 on address 4)
---				X"AC030006", --sw $3, 6($0)		/Saving value 12 on address 6	
---				X"AC030007", --sw $3, 7($0)		/Saving value 12 on address 7	
---				X"3C030006", --lui $3, 6			/$3 = 6 * 2^16 = 393216 = 0x60000
---				X"AC030008", --sw $3, 8($0)		/Saving value 0x60000 on address 8	
---				X"00231820", --add $3, $1, $3		/$3 = 393218 = 0x60002	
---				X"AC030009", --sw $3, 9($0)		/Saving 0x60002 on address 9	
---				X"10400002", --beq $2, $0, 2		/No branch	
---				X"0001982A", --slt $19, $0, $1	/$19 = 1		
---				X"AC13000C", --sw $19, 12($0)		/Saving 1 on address 12	
---				X"08000013", --j 19					/jump to 19
---				X"AC030001", --sw $3, 1($0)		/SKIPPED (Saving 0x60002 on address 1)	
---				X"1000FFFD", --beq $0, $0, -3		/SKIPPED (Branch back three steps)	
---				X"00622022", --sub $4, $3, $2		/$4 = 0x5FFF8 	
---				X"00822022", --sub $4, $4, $2		/$4 = 0x5FFEE
---				X"AC04000D", --sw $4, 13($0)		/Saving value 0x5FFEE on address 13 	
---				X"00221820", --add $3, $1, $2		/$3 = 12	
---				X"00432024", --and $4, $2, $3		/$4 = 1000 = 8	
---				X"00432825", --or $5, $2, $3		/$5 = 1110 = 14	
---				X"AC04000F", --sw $4, 15($0)		/Saving value 8 on address 15	
---				X"AC050010", --sw $5, 16($0)		/Saving value 14 (= 0xE) on address 16	
---				X"002A5020", --add $10, $1, $10  /add $1 to $ 10 and place in $10
---				X"1000FFFF", --beq $0, $0, -1	/Branch back one step to hold off code at this spot
---				X"AC050012" --sw $5, 18($0)		/SHOULD NEVER HAPPEN (Saving value 14 (= 0xE) on address 18.)
---				);
 		begin
 			for i in 0 to TEST_INSTRS-1 loop
 				WriteInstructionWord(TestInstrData(i), to_unsigned(i, ADDR_WIDTH));
@@ -238,6 +212,7 @@ DataMem:			entity work.DualPortMem port map (
 		begin
 			wait until processor_enable = '0';
 			-- expected data memory contents, derived from program behavior
+			CheckDataWord(x"00000000", 1);
 			CheckDataWord(x"00000002", 2);
 			CheckDataWord(x"00000003", 3);
 			CheckDataWord(x"00000001", 4);
